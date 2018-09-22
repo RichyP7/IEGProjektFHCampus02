df26b66ce7c46308cc75523298e3ce878f0fd92e
KevinGolob <kevin.golob@hotmail.de>
2018-09-22 11:42:53 +0200

    add hardcoded product service

---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Products.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Coca Cola", "Fanta", "Sprite", "Soda" };
+        }
+        
+    }
+}
