05390c5f4e1c52373945c35446c3ab868b9803f9
fschmuck <florian.schmuck@bearingpoint.com>
2019-01-12 14:46:17 +0100

    [SecretService] - added JWTAuthentication Service

---
+++ JWTAuthentication/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Models
+{
+    public class UserModel
+    {
+        private string username;
+        private string emailaddress;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        
+
+        public string EmailAddress
+        {
+            get { return emailaddress; }
+            set { emailaddress = value; }
+        }
+
+
+    }
+}
