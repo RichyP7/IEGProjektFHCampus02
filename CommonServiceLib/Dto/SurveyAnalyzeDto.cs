4903fe6491b8dfe48ad3d611d659fa222fbfaf9e
agines <agines.dev@gmx.at>
2019-01-12 14:47:06 +0100

    NOTASK: survey publish controller

---
+++ CommonServiceLib/Dto/SurveyAnalyzeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonServiceLib.Dto
+{
+    public class SurveyAnalyzeDto
+    {
+        public int SurveyCount { get; set; }
+        public int OverallQuestionCount { get; set; }
+    }
+}
