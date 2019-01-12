a81fb61ed34a28f4676403d83228571058619790
fschmuck <florian.schmuck@bearingpoint.com>
2019-01-12 15:16:19 +0100

    [SurveyAnalyzeService] - added analyzeSurvey

---
+++ SurveyAnalyzeService/Controllers/SurveyAnalyzeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonServiceLib.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyAnalyzeService.Controllers
+{
+    [Route("api/surveys/analyze")]
+    public class SurveyAnalyzeController : Controller
+    {
+        [HttpPost]
+        public ActionResult<SurveyAnalyzeDto> AnalyzeSurveys(List<SurveyDto> surveys)
+        {
+            return Ok(Analyze(surveys));
+        }
+
+        private SurveyAnalyzeDto Analyze(List<SurveyDto> surveys)
+        {
+            SurveyAnalyzeDto analyzeDto = new SurveyAnalyzeDto();
+            int questions = 0;
+            surveys.ForEach(it => questions += it.SurveyQuestions.Count);
+            analyzeDto.OverallQuestionCount = questions;
+            analyzeDto.SurveyCount = surveys.Count;
+            return analyzeDto;
+        }
+    }
+}
