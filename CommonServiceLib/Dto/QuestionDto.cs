1c5a259245c38f2a5b5bfdadae61d739b04052a6
Putz Richard <richard.putz@evon-automation.com>
2019-01-12 14:14:58 +0100

    add dto for Survey

---
+++ CommonServiceLib/Dto/QuestionDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonServiceLib.Dto
+{
+    public class QuestionDto
+    {
+        public string QuestionText { get; set; }
+        public List<AnswerDto> Answers{ get; set; }
+    }
+}
---
+++ CommonServiceLib/Dto/SurveyDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonServiceLib.Dto
+{
+    public class SurveyDto
+    {
+        public int Id { get; set; }
+        List<QuestionDto> SurveyQuestions { get; set; }
+    }
+}
