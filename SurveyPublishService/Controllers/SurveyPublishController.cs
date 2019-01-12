b992498d6a3b5df3a81ad1e7500cfdf35bb8eb45
agines <agines.dev@gmx.at>
2019-01-12 15:08:41 +0100

    NOTASK: survey publish controller logic

--- SurveyPublishService/Controllers/SurveyPublishController.cs
+++ SurveyPublishService/Controllers/SurveyPublishController.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using CommonServiceLib.Discovery;
 using CommonServiceLib.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace SurveyPublishService.Controllers
 {
     
 
-    [Route("api/surveys")]
+    [Route("api/surveys/publish")]
     [ApiController]
     public class SurveyPublishController : ControllerBase
     {
@@ -19,11 +21,30 @@ namespace SurveyPublishService.Controllers
         [HttpGet]
         public ActionResult<SurveyAnalyzeDto> Get()
         {
-            string surveyCreatorService = apiClient.GetLoadBalancedUrl("survey-creator-service");
-            //TODO: get all from surveys from survey creator
-            //TODO: send all surveys to survey analyzer
-            //TODO: get response and return to client
-            return new SurveyAnalyzeDto();
+            List<SurveyDto> surveys = GetSurveys();
+            return GetAnalyzedSurveys(surveys);
+        }
+
+
+        private List<SurveyDto> GetSurveys() {
+            List<SurveyDto> surveys = null;
+            HttpResponseMessage response = apiClient.client.GetAsync(apiClient.GetLoadBalancedUrl("survey-creator-service") + " /api/surveys").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                surveys = response.Content.ReadAsAsync<List<SurveyDto>>().Result;
+            }
+            return surveys;
+        }
+
+        private SurveyAnalyzeDto GetAnalyzedSurveys(List<SurveyDto> surveys)
+        {
+            SurveyAnalyzeDto surveyAnalyze = null;
+            HttpResponseMessage response = apiClient.client.PostAsync(apiClient.GetLoadBalancedUrl("survey-analyze-service") + "/api/surveys/analyze", new StringContent(JObject.FromObject(surveys).ToString())).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                surveyAnalyze = response.Content.ReadAsAsync<SurveyAnalyzeDto>().Result;
+            }
+            return surveyAnalyze;
         }
     }
 }
